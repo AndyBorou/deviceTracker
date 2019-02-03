5a5f4db89da959afb46d862131229374945335a8
AndyBoro <borovoy334@gmail.com>
2019-02-03 23:43:04 +0200

    Revert "add 20:09"

--- src/main/java/com/waverley/tracker/tesstDir/Test.java
+++ src/main/java/com/waverley/tracker/tesstDir/Test.java
@@ -4,7 +4,7 @@ package com.waverley.tracker.tesstDir;
  * Created by Andrey on 2/2/2019.
  */
 public class Test {
-    String i = "23:09";
+    int i =3;
     String i2 = "23:19";
     String i2i = "23:24";
 
